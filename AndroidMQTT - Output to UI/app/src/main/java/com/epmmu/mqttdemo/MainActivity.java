85faf9f9d49215e064e3e27453736b2f9f6e3531
Piotr Zadka <piotr.zadka@gmail.com>
2018-11-11 00:59:36 +0000

    Next commit
    Mostly done.
    Need to add better UI

--- AndroidMQTT - Output to UI/app/src/main/java/com/epmmu/mqttdemo/MainActivity.java
+++ AndroidMQTT - Output to UI/app/src/main/java/com/epmmu/mqttdemo/MainActivity.java
@@ -3,14 +3,18 @@ package com.epmmu.mqttdemo;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.MqttTopic;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,12 +31,30 @@ public class MainActivity extends AppCompatActivity {
     String sensorname = "doorState";
     String topicname = userid + "/" + sensorname;
 
+    public final String SWITCH_STATE = userid +"/doorState";
+
     private MqttClient mqttClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Switch mainSwitch = findViewById(R.id.mainSwitch);
+        mainSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mainSwitch.isChecked()){
+                    publishSwitchState("open");
+                    Toast.makeText(MainActivity.this, "Main Door Open", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    publishSwitchState("close");
+                    Toast.makeText(MainActivity.this, "Main Door Closed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
 
         // start new
         // Create MQTT client and start subscribing to message queue
@@ -55,7 +77,6 @@ public class MainActivity extends AppCompatActivity {
                         public void run() {
                             System.out.println("Updating UI");
                             // Update UI elements
-                            Switch mainSwitch = findViewById(R.id.mainSwitch);
                             if(messageStr.equals("open")){
                                 mainSwitch.setChecked(true);
                             }
@@ -109,6 +130,22 @@ public class MainActivity extends AppCompatActivity {
             System.exit(1);
         }
     }
+
+    private void publishSwitchState(String switchState) {
+        try {
+            final MqttTopic switchStateTopic = mqttClient.getTopic(SWITCH_STATE);
+
+            switchStateTopic.publish(new MqttMessage(switchState.getBytes()));
+
+            System.out.println("Published data. Topic: " + switchStateTopic.getName() + "  Message: " + switchState);
+
+        } catch (MqttException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+
 
 
 }
